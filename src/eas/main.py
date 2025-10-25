fb6afb47bcb41f86a98f91d703988e6d535c7844
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2025-10-25 20:14:14 +0100

    Refactor pins

--- src/eas/main.py
+++ src/eas/main.py
@@ -8,18 +8,8 @@ from enum import Enum
 SCREEN_HEIGHT_MM = 133
 SCREEN_WIDTH_MM = 185
 
-x_in1 = 17
-x_in2 = 18
-x_in3 = 27
-x_in4 = 22
-
-y_in1 = 5
-y_in2 = 6
-y_in3 = 12
-y_in4 = 13
-
-x_motor_pins = [x_in1, x_in2, x_in3, x_in4]
-y_motor_pins = [y_in1, y_in2, y_in3, y_in4]
+X_MOTOR_PINS = [17, 18, 27, 22]
+Y_MOTOR_PINS = [5, 6, 12, 13]
 
 STEP_SLEEP_SECS = 0.002  # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
 STEPS_PER_TURN = 4096  # 5.625*(1/64) per step, 4096 steps is 360°
@@ -49,53 +39,50 @@ class Direction(Enum):
 def setup_gpio():
     # set up pins
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(x_in1, GPIO.OUT)
-    GPIO.setup(x_in2, GPIO.OUT)
-    GPIO.setup(x_in3, GPIO.OUT)
-    GPIO.setup(x_in4, GPIO.OUT)
 
-    GPIO.setup(y_in1, GPIO.OUT)
-    GPIO.setup(y_in2, GPIO.OUT)
-    GPIO.setup(y_in3, GPIO.OUT)
-    GPIO.setup(y_in4, GPIO.OUT)
+    for pin in X_MOTOR_PINS:
+        GPIO.setup(pin, GPIO.OUT)
+
+    for pin in Y_MOTOR_PINS:
+        GPIO.setup(pin, GPIO.OUT)
 
     # initialize to low
-    GPIO.output(x_in1, GPIO.LOW)
-    GPIO.output(x_in2, GPIO.LOW)
-    GPIO.output(x_in3, GPIO.LOW)
-    GPIO.output(x_in4, GPIO.LOW)
+    for pin in X_MOTOR_PINS:
+        GPIO.output(pin, GPIO.LOW)
 
-    GPIO.output(y_in1, GPIO.LOW)
-    GPIO.output(y_in2, GPIO.LOW)
-    GPIO.output(y_in3, GPIO.LOW)
-    GPIO.output(y_in4, GPIO.LOW)
+    for pin in Y_MOTOR_PINS:
+        GPIO.output(pin, GPIO.LOW)
 
 
 def cleanup_gpio():
-    GPIO.output(x_in1, GPIO.LOW)
-    GPIO.output(x_in2, GPIO.LOW)
-    GPIO.output(x_in3, GPIO.LOW)
-    GPIO.output(x_in4, GPIO.LOW)
+    for pin in X_MOTOR_PINS:
+        GPIO.output(pin, GPIO.LOW)
 
-    GPIO.output(y_in1, GPIO.LOW)
-    GPIO.output(y_in2, GPIO.LOW)
-    GPIO.output(y_in3, GPIO.LOW)
-    GPIO.output(y_in4, GPIO.LOW)
+    for pin in Y_MOTOR_PINS:
+        GPIO.output(pin, GPIO.LOW)
 
     GPIO.cleanup()
 
 
 def main():
-    spin_motor(STEPS_PER_TURN, x_dir=Direction.ZERO, y_dir=Direction.POSITIVE)
+    spin_motor(STEPS_PER_TURN, x_dir=Direction.POSITIVE, y_dir=Direction.POSITIVE)
 
 
 def spin_motor(step_count: int, x_dir: Direction, y_dir: Direction):
     global x_motor_sequence_index, y_motor_sequence_index
 
     for _ in range(step_count):
-        for pin in range(0, 4):
-            GPIO.output(x_motor_pins[pin], STEP_SEQUENCE[x_motor_sequence_index][pin])
-            GPIO.output(y_motor_pins[pin], STEP_SEQUENCE[y_motor_sequence_index][pin])
+        if x_dir != Direction.ZERO:
+            for pin in range(0, 4):
+                GPIO.output(
+                    X_MOTOR_PINS[pin], STEP_SEQUENCE[x_motor_sequence_index][pin]
+                )
+
+        if y_dir != Direction.ZERO:
+            for pin in range(0, 4):
+                GPIO.output(
+                    Y_MOTOR_PINS[pin], STEP_SEQUENCE[y_motor_sequence_index][pin]
+                )
 
         match x_dir:
             case Direction.NEGATIVE:
