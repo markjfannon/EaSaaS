49e782186614950ff0a5f4f6f1dad2cef7968f22
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2025-10-25 23:00:44 +0100

    Add backlash compensation

--- src/eas/main.py
+++ src/eas/main.py
@@ -13,6 +13,7 @@ Y_MOTOR_PINS = [5, 6, 12, 13]
 
 STEP_SLEEP_SECS = 0.002  # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
 STEPS_PER_TURN = 4096  # 5.625*(1/64) per step, 4096 steps is 360°
+BACKLASH_COMPENSATION_STEPS = 200  # Number of steps needed to compensate for backlash
 
 # stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
 STEP_SEQUENCE = [
@@ -26,14 +27,18 @@ STEP_SEQUENCE = [
     [0, 0, 0, 1],
 ]
 
-x_motor_sequence_index = 0
-y_motor_sequence_index = 0
-
 
 class Direction(Enum):
     POSITIVE = 1
     ZERO = 0
     NEGATIVE = -1
+
+
+x_motor_sequence_index = 0
+y_motor_sequence_index = 0
+
+current_x_dir = Direction.ZERO
+current_y_dir = Direction.ZERO
 
 
 def setup_gpio():
@@ -65,43 +70,58 @@ def cleanup_gpio():
 
 
 def main():
-    spin_motor(STEPS_PER_TURN, x_dir=Direction.POSITIVE, y_dir=Direction.ZERO)
-    spin_motor(STEPS_PER_TURN, x_dir=Direction.ZERO, y_dir=Direction.POSITIVE)
-    spin_motor(STEPS_PER_TURN, x_dir=Direction.NEGATIVE, y_dir=Direction.ZERO)
-    spin_motor(STEPS_PER_TURN, x_dir=Direction.ZERO, y_dir=Direction.NEGATIVE)
+    spin_motor(STEPS_PER_TURN, x_dir=Direction.POSITIVE, y_dir=Direction.NEGATIVE)
+    spin_motor(STEPS_PER_TURN, x_dir=Direction.POSITIVE, y_dir=Direction.POSITIVE)
+    spin_motor(STEPS_PER_TURN, x_dir=Direction.NEGATIVE, y_dir=Direction.POSITIVE)
+    spin_motor(STEPS_PER_TURN, x_dir=Direction.NEGATIVE, y_dir=Direction.NEGATIVE)
 
 
 def spin_motor(step_count: int, x_dir: Direction, y_dir: Direction):
     global x_motor_sequence_index, y_motor_sequence_index
+    global current_x_dir, current_y_dir
 
-    for _ in range(step_count):
-        if x_dir != Direction.ZERO:
+    x_fuel = step_count
+    y_fuel = step_count
+
+    if current_x_dir != Direction.ZERO and current_x_dir != x_dir:
+        x_fuel += BACKLASH_COMPENSATION_STEPS
+
+    if current_y_dir != Direction.ZERO and current_y_dir != y_dir:
+        y_fuel += BACKLASH_COMPENSATION_STEPS
+
+    if x_dir != Direction.ZERO:
+        current_x_dir = x_dir
+    if y_dir != Direction.ZERO:
+        current_y_dir = y_dir
+
+    while x_fuel > 0 or y_fuel > 0:
+        if x_dir != Direction.ZERO and x_fuel >= y_fuel:
+            x_fuel -= 1
+
             for pin in range(0, 4):
                 GPIO.output(
                     X_MOTOR_PINS[pin], STEP_SEQUENCE[x_motor_sequence_index][pin]
                 )
 
-        if y_dir != Direction.ZERO:
+            match x_dir:
+                case Direction.NEGATIVE:
+                    x_motor_sequence_index = (x_motor_sequence_index + 1) % 8
+                case Direction.POSITIVE:
+                    x_motor_sequence_index = (x_motor_sequence_index - 1) % 8
+
+        if y_dir != Direction.ZERO and y_fuel >= x_fuel:
+            y_fuel -= 1
+
             for pin in range(0, 4):
                 GPIO.output(
                     Y_MOTOR_PINS[pin], STEP_SEQUENCE[y_motor_sequence_index][pin]
                 )
 
-        match x_dir:
-            case Direction.NEGATIVE:
-                x_motor_sequence_index = (x_motor_sequence_index + 1) % 8
-            case Direction.POSITIVE:
-                x_motor_sequence_index = (x_motor_sequence_index - 1) % 8
-            case Direction.ZERO:
-                pass
-
-        match y_dir:
-            case Direction.NEGATIVE:
-                y_motor_sequence_index = (y_motor_sequence_index - 1) % 8
-            case Direction.POSITIVE:
-                y_motor_sequence_index = (y_motor_sequence_index + 1) % 8
-            case Direction.ZERO:
-                pass
+            match y_dir:
+                case Direction.NEGATIVE:
+                    y_motor_sequence_index = (y_motor_sequence_index - 1) % 8
+                case Direction.POSITIVE:
+                    y_motor_sequence_index = (y_motor_sequence_index + 1) % 8
 
         time.sleep(STEP_SLEEP_SECS)
 
